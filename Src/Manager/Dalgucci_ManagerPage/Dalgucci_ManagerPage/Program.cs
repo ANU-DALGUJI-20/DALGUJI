28b091ec65c006b9ef8dc97e4def64c8925b7bd6
pusisi1 <pusisi1@naver.com>
2020-10-27 11:10:24 +0900

    park manager

---
+++ Src/Manager/Dalgucci_ManagerPage/Dalgucci_ManagerPage/Program.cs
@@ -0,0 +1,26 @@
+﻿using Dalgucci;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dalgucci_ManagerPage
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        static public Database data = null;
+        [STAThread]
+       
+        static void Main()
+        {
+            data = new Database();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
