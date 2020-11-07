53dcb8297a21b8549f57be9151464aa1cee6d55d
pusisi1 <pusisi1@naver.com>
2020-11-07 15:51:04 +0900

    123

--- Src/Manager/Dalgucci_ManagerPage/Dalgucci_ManagerPage/DataContext/Database.cs
+++ Src/Manager/Dalgucci_ManagerPage/Dalgucci_ManagerPage/DataContext/Database.cs
@@ -16,7 +16,7 @@ namespace Dalgucci
 
         public Database()
         {
-            string strConn = "Server=127.0.0.1;Database=SF1team;User Id=sa;Password=1234;";
+            string strConn = "Server=127.0.0.1;Database=SF1team;User Id=sa;Password=0924;";
             conn = new SqlConnection(strConn);
             conn.Open();
         }
